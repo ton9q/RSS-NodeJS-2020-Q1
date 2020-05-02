a9a136feeb70a4071085aaf5bd9cc2430723a55d
Anton Kuchma <kuchma_anton@mail.ru>
2020-05-02 18:21:40 +0300

    remove trash

--- task-5/src/modules/authentication/authentication.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const asyncWrapper = require('../../utils/async-wrapper');
-const authenticationController = require('./authentication.controller');
-
-router.route('/login').post(asyncWrapper(authenticationController.signIn));
-
-module.exports = router;
