80199e0f8b4500f33ac9820871f9de7f9e7540fe
Anton Kuchma <kuchma_anton@mail.ru>
2022-03-17 20:13:48 +0400

    feat: set up typedoc, add jsdocs for user and user.db.repository files

--- task-6/src/modules/user/user.db.repository.ts
+++ task-6/src/modules/user/user.db.repository.ts
@@ -1,31 +1,66 @@
+/**
+ * Module for all User DB related functions.
+ * @module UserDBRepository
+ */
+
 import User from './user';
 import UserModel, { IUserDocument } from './user.model';
 
-export const getAll = async () => {
+/**
+ * Get Array of all users
+ * @returns {Promise<IUserDocument[]>} array of user documents
+ */
+export const getAll = async (): Promise<IUserDocument[]> => {
   const users = await UserModel.find({});
   return users;
 };
 
+/**
+ * Get User by login field
+ * @param {string} login - user login
+ * @returns {Promise<IUserDocument | null>} user document
+ */
 export const getByLogin = async (login: string): Promise<IUserDocument | null> => {
   const user = await UserModel.findOne({ login });
   return user;
 };
 
+/**
+ * Get User by id field
+ * @param {string} id - user id
+ * @returns {Promise<IUserDocument | null>} user document
+ */
 export const getById = async (id: string): Promise<IUserDocument | null> => {
   const user = await UserModel.findById(id);
   return user;
 };
 
+/**
+ * Add user to DB
+ * @param {User} newUserData - instance of user
+ * @returns {Promise<IUserDocument>} user documents
+ */
 export const add = async (newUserData: User): Promise<IUserDocument> => {
   const newUser = await UserModel.create(newUserData);
   return newUser;
 };
 
+/**
+ * Update user by id
+ * @param {string} id - user id
+ * @param {Partial<User>} userData - user data to update
+ * @returns {Promise<IUserDocument>} updated user document
+ */
 export const updateById = async (id: string, userData: Partial<User>): Promise<IUserDocument> => {
   const updatedUser = await UserModel.updateOne({ _id: id }, userData);
   return updatedUser;
 };
 
+/**
+ * Delete user by id
+ * @param {string} id - user id
+ * @returns {Promise<IUserDocument | null>} data of deleted user
+ */
 export const deleteById = async (id: string): Promise<IUserDocument | null> => {
   const deletedUser = await UserModel.deleteOne({ _id: id });
   return deletedUser;
--- task-6/src/modules/user/user.ts
+++ task-6/src/modules/user/user.ts
@@ -1,31 +1,76 @@
+/**
+ * Module for User class.
+ * @module User
+ */
+
 import { filterFields } from '../../utils/object';
 
+/** @type {string[]} array of allowed fields for user */
 const fieldsToInclude = ['name', 'login', 'password'];
 
+/**
+ * Interface for User class.
+ *
+ * @interface
+ */
 export interface IUser {
   name: string;
   login: string;
   password: string;
 }
 
+/**
+ * Class representing a user.
+ * @class
+ * @constructor
+ * @public
+ */
 class User {
+  /**
+   * @public
+   * @property name {string} Name of a user
+   */
   name: string;
 
+  /**
+   * @public
+   * @property login {string} login of a user
+   */
   login: string;
 
+  /**
+   * @public
+   * @property password {string} password of a user
+   */
   password: string;
 
+  /**
+   * Create a user.
+   * @param {IUser} user - object with user fields
+   */
   constructor({ name, login, password }: IUser) {
     this.name = name;
     this.login = login;
     this.password = password;
   }
 
+  /**
+   * Get user with public fields.
+   * @static
+   * @param {User} user - user object
+   * @return Object contains public user fields.
+   */
   static toResponse(user: User) {
     const { name, login } = user;
     return { name, login };
   }
 
+  /**
+   * Filter user fields and store only allowed.
+   * @static
+   * @param {Partial<IUser>} data - user data that should be filtered
+   * @return {User} filtered user
+   */
   static filterFields(data: Partial<IUser>) {
     return filterFields(data, fieldsToInclude);
   }
